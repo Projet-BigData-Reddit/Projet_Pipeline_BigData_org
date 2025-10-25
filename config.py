9eefeb213d039065e005576c932382e6c5fa3c4c
hamza <hamzachatraoui4@gmail.com>
2025-10-25 01:58:29 +0100

    modif kafka +ajout file for ingestion

--- config.py
+++ config.py
@@ -9,4 +9,16 @@ USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 
 KAFKA_TOPIC = "Reddit_Data"
+KAFKA_BROKER_URL = 'localhost:9092'
+PARTITIONS = 1
+REPLICATION_FACTOR = 1
+
+SUBREDDITS = "cryptocurrency+Bitcoin+Ethereum+altcoin+CryptoMarkets+ethtrader+CryptoTechnology+CryptoCurrencyNews+DeFi+CryptoMoonShots+Dogecoin+Cardano+Solana+ShibaInu"
+KEYWORDS = [
+    "crypto", "cryptocurrency", "bitcoin", "btc", "ethereum", "eth",
+    "blockchain", "altcoin", "token", "defi", "nft", "smart contract",
+    "mining", "miner", "hash rate", "wallet", "hardware wallet", "cold storage",
+    "staking", "yield farming", "airdrop", "ico", "ido", "web3",
+    "dogecoin", "doge", "cardano", "ada", "solana", "sol", "shiba inu", "shib"
+]
 
