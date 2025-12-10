5ae78ec49b4c0b1e1291bee81f01446bfe42fab1
medamineelrherbi <medamineelrherbi@gmail.com>
2025-12-10 19:33:18 +0100

    ajout des commentaires

--- distilbert_fin/app.py
+++ distilbert_fin/app.py
@@ -11,11 +11,15 @@ classifier = pipeline(
 
 app = FastAPI()
 
+'''
+class InputData(BaseModel): : On crée une classe nommée InputData qui hérite de BaseModel (de la librairie Pydantic). elle est utilisée pour valider les données.
+texts : list : on spécifie que l'entrée doit contenir un champ applé texts qui doit être une liste
+'''
 class InputData(BaseModel):
     texts: list
 
-@app.post("/predict")
-def predict(data: InputData):
+@app.post("/predict") # dit a FastAPI : quand quelqu'un fait un POST vers /predict, exécute cette fonction
+def predict(data: InputData): #La fonction prend en entrée les données envoyées par l'utilisateur, et grâce à InputData, elle sait que c'est un objet contenant une liste appelée texts.
     results = classifier(data.texts)
     labels = [r["label"] for r in results]
     return {"labels": labels}
